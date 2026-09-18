eb129345e4ad069d17e34441603f351b69b4e154
root <root@localhost.localdomain>
2026-09-18 02:13:03 +0100

    Fix responsive pitch sizing for landscape

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1398200868" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "747816171" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
